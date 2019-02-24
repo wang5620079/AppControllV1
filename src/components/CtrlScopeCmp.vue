41ce805c3ff288b5483aa6f82ec222ee25dbb4c8
win-pc <wang5620079@163.com>
2019-02-25 01:25:40 +0800

    增加mock的随机系统名称生成 其他略微调整 完善系统选择框和部署选择框的逻辑

--- src/components/CtrlScopeCmp.vue
+++ src/components/CtrlScopeCmp.vue
@@ -14,13 +14,13 @@
             showSearch
             placeholder="请选择系统"
             optionFilterProp="children"
-            style="width: 200px"
-            @focus="handleFocus"
-            @blur="handleBlur"
-            @change="handleChange"
+            style="width: 230px"
+            mode="multiple"
+            @change="handleappinfoChange"
             :filterOption="filterOption"
           >
-            <a-select-option :value=appinfo.appname v-for="appinfo in appinfos" :key="appinfo.id" >{{appinfo.appname}}</a-select-option>
+            <a-select-option :value=appinfo.id v-for="appinfo in appinfos" :key="appinfo.id">{{appinfo.appname}}
+            </a-select-option>
           </a-select>
         </a-form-item>
         <a-form-item
@@ -32,15 +32,17 @@
             showSearch
             placeholder="请选择部署方式"
             optionFilterProp="children"
-            style="width: 200px"
+            mode="multiple"
+            style="width: 230px"
             @focus="handleFocus"
             @blur="handleBlur"
-            @change="handleChange"
+            @change="handledeplomentChange"
             :filterOption="filterOption"
+            :value="selecteddpls"
           >
-            <a-select-option value="总部">总部</a-select-option>
-            <a-select-option value="北京">北京</a-select-option>
-            <a-select-option value="山东">山东</a-select-option>
+            <a-select-option :value=deployment.id v-for="deployment in deploymentoptions" :key="deployment.id">
+              {{deployment.name}}
+            </a-select-option>
           </a-select>
         </a-form-item>
         <a-form-item>
@@ -53,8 +55,26 @@
         </a-form-item>
       </a-form>
     </div>
+    <!--穿梭框-->
     <div>
-      <p>这里配置穿梭框</p>
+      <a-transfer
+        :dataSource="nodesdata"
+        showSearch
+        :listStyle="{
+      width: '305px',
+      height: '300px',
+    }"
+        :operations="['to right', 'to left']"
+        :targetKeys="targetnodes"
+        @change="handlenodesChange"
+        :render="item=>`${item.title}-${item.description}`"
+      >
+        <span
+          slot="notFoundContent"
+        >
+      请选择节点
+    </span>
+      </a-transfer>
     </div>
   </div>
 </template>
@@ -63,11 +83,15 @@ export default {
   name: 'CtrlScopeCmp',
   data () {
     return {
+      form: this.$form.createForm(this),
       appinfos: [],
-      form: this.$form.createForm(this)
+      deploymentoptions: [],
+      selecteddpls: [],
+      nodesdata: [],
+      targetnodes: []
     }
   },
-  created: function () {
+  mounted: function () {
     var me = this
     this.axios.post('http://127.0.0.1/queryrandapppnames').then((response) => {
       me.appinfos = response.data.appinfos
@@ -75,8 +99,47 @@ export default {
     })
   },
   methods: {
-    handleChange (value) {
+    handleappinfoChange (value) {
+      console.log('**************handleappinfoChange***************')
+      var me = this
       console.log(`selected ${value}`)
+      // 如果什么选项都没选，就清空部署选择框及其已选的值
+      if (value.length === 0) {
+        this.deploymentoptions = []
+        this.selecteddpls = []
+        return
+      }
+      // 如果有选择就设置部署下拉框的选项及已选的值
+      this.axios.post('http://127.0.0.1/querydevelopment', JSON.stringify(value)).then((response) => {
+        me.deploymentoptions = response.data.data
+        console.log(response.data.data)
+        console.log(me.selecteddpls)
+        // 如果没有已选项，就返回
+        if (me.selecteddpls.length === 0) {
+          return
+        }
+        // 如果有已选项，且已选项中有deploymentoptions没有的选项A，那就把已选的这个选项A删除掉
+        let tmparr = []
+        let depoptsids = []
+        for (var item of me.deploymentoptions) {
+          depoptsids.push(item.id)
+        }
+        me.selecteddpls.forEach((item, index) => {
+          if (!depoptsids.includes(item)) {
+            tmparr.push(item)
+          }
+        })
+        tmparr.forEach((item, index) => {
+          me.selecteddpls.splice(me.selecteddpls.indexOf(item), 1)
+        })
+      })
+    },
+    handledeplomentChange (value) {
+      console.log('**************handledeplomentChange***************')
+      console.log(value)
+      console.log(this.selecteddpls)
+      this.selecteddpls = value
+      console.log(this.selecteddpls)
     },
     handleBlur () {
       console.log('blur')
@@ -84,8 +147,17 @@ export default {
     handleFocus () {
       console.log('focus')
     },
+    onChange (e) {
+      for (let idx in this.appinfos) {
+        let tmp = this.appinfos[idx]
+        if (tmp.id === e.target.value) {
+          console.log(tmp.state)
+        }
+      }
+    },
     filterOption (input, option) {
       return option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      // return option.componentOptions.propsData.value.toLowerCase().indexOf(input.toLowerCase()) >= 0
     },
     queryappinfos () {
       var me = this
--- src/mock/index.js
+++ src/mock/index.js
@@ -5,14 +5,13 @@ const mocktest = Mock.mock('http://127.0.0.1/main.php?cid=1', 'post', () => {
 })
 
 const appnames = Mock.mock('http://127.0.0.1/queryapppnames', 'post', () => {
-  return MyInfo.appnames
+  return randappnames
 })
 
 const randappnames = Mock.mock('http://127.0.0.1/queryrandapppnames', 'post', () => {
-  const Random = Mock.Random
   let arr = []
   for (let i = 0; i < 100; i++) {
-    arr.push({id: 'app' + i, appname: Random.cname()})
+    arr.push({ id: 'app' + i, appname: '系统' + i, state: false })
   }
   var retobj = {
     code: '200',
@@ -22,4 +21,42 @@ const randappnames = Mock.mock('http://127.0.0.1/queryrandapppnames', 'post', ()
   return retobj
 })
 
-export default { mocktest, appnames, randappnames }
+const querynodes = Mock.mock('http://127.0.0.1/querynodes', 'post', () => {
+  let arr = []
+  for (let i = 0; i < 100; i++) {
+    arr.push({ id: 'app' + i, appname: '系统' + i, state: false })
+  }
+  var retobj = {
+    code: '200',
+    appinfos: arr
+  }
+  console.log(retobj)
+  return retobj
+})
+
+const querydevelopment = Mock.mock('http://127.0.0.1/querydevelopment', 'post', function (options) {
+  let tmpset = new Set()
+  let optionsarr = JSON.parse(options.body)
+  if (optionsarr === undefined || optionsarr.length === 0) {
+    // tmpset.add({id: 'dep0', name: '南中心'})
+    return {code: '200', data: Array.from(tmpset)}
+  }
+  let developmentsmap = new Map([['app0', {id: 'dep0', name: '南中心'}], ['app1', {id: 'dep1', name: 'IDC机房'}], ['app2', {id: 'dep2', name: '中电投'}]])
+  optionsarr.forEach((item, index) => {
+    if (item === 'app0') {
+      tmpset.add(developmentsmap.get('app0'))
+    }
+    if (item === 'app1') {
+      tmpset.add(developmentsmap.get('app1'))
+    }
+    if (item === 'app2') {
+      tmpset.add(developmentsmap.get('app2'))
+    }
+    if (item > 'app2' && !optionsarr.includes('app0')) {
+      tmpset.add({id: 'dep0', name: '南中心'})
+    }
+  })
+  return {code: '200', data: Array.from(tmpset)}
+})
+
+export default { mocktest, appnames, randappnames, querydevelopment, querynodes }

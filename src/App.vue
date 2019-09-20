847c295e881f59df8362b97f9ea162f9662af6c6
wp <wang5620079@163.com>
2019-09-20 21:42:59 +0800

    第一次提交，增加TestCmp

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,54 @@
 <template>
-  <a-layout id="components-layout-demo-custom-trigger">
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
     <a-layout-sider
-      :trigger="null"
       collapsible
       v-model="collapsed"
     >
-      <div class="logo"/>
-      <a-menu
-        theme="dark"
-        mode="inline"
-        :defaultSelectedKeys="['1']"
-        :inlineCollapsed="collapsed"
-        :defaultOpenKeys="['servicectrl']"
-        @click="handleClick"
-      >
-        <a-sub-menu key="syssetup">
-          <span slot="title"><a-icon type="folder" /><span>系统设置</span></span>
-          <a-menu-item key="paramsetting"><router-link to="/TestCmp">参数设置</router-link></a-menu-item>
-          <a-menu-item key="runtimesetting"><router-link to="/runtimesetting">运行设置</router-link></a-menu-item>
+      <div class="logo"></div>
+      <a-menu theme="dark" :defaultSelectedKeys="['3']" mode="inline" defaultOpenKeys= "['sub1' 'sub2']">
+        <a-sub-menu
+          key="sub1" @click="handleMenuClick"
+        >
+          <span slot="title"><a-icon type="user" /><span>参数设置</span></span>
+          <a-menu-item key="1">系统参数设置</a-menu-item>
+          <a-menu-item key="2">运行参数设置</a-menu-item>
         </a-sub-menu>
-        <a-sub-menu key="servicectrl">
-          <span slot="title"><a-icon type="folder" /><span>URL监控</span></span>
-          <a-menu-item key="monitorstatus"><router-link to="/monitorstatus">监控情况</router-link></a-menu-item>
+        <a-sub-menu
+          key="sub2"
+        >
+          <span slot="title"><a-icon type="user" /><span>监控情况</span></span>
+          <a-menu-item key="3">url监控情况</a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="()=> collapsed = !collapsed"
-        />
-      </a-layout-header>
-      <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff' }">
-        <router-view></router-view>
+      <a-layout-content style="margin: 0 16px">
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '720px' }">
+          <router-view></router-view>
+        </div>
       </a-layout-content>
     </a-layout>
   </a-layout>
 </template>
 <script>
-
 export default {
-  name: 'App',
   data () {
     return {
       collapsed: false
     }
   },
   methods: {
-    toggleCollapsed () {
-      this.collapsed = !this.collapsed
-    },
-    handleClick (e) {
+    handleMenuClick (e) {
       console.log('click', e)
-    },
-    titleClick (e) {
-      console.log('titleClick', e)
-    }
-  },
-  watch: {
-    openKeys (val) {
-      console.log('openKeys', val)
     }
   }
 }
 </script>
+
 <style>
-  #components-layout-demo-custom-trigger .trigger {
-    font-size: 18px;
-    line-height: 64px;
-    padding: 0 24px;
-    cursor: pointer;
-    transition: color .3s;
-  }
-
-  #components-layout-demo-custom-trigger .trigger:hover {
-    color: #1890ff;
-  }
-
-  #components-layout-demo-custom-trigger .logo {
+  #components-layout-demo-side .logo {
     height: 32px;
-    background: rgba(255, 255, 255, .2);
+    background: rgba(255,255,255,.2);
     margin: 16px;
   }
 </style>
--- src/components/TestCmp.vue
+++ src/components/TestCmp.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script>
-    export default {
-        name: "TestCmp"
-    }
+export default {
+  name: 'TestCmp'
+}
 </script>
 
 <style scoped>
